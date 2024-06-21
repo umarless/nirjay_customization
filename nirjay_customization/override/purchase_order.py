8f78db2e2f79a5b3906d35c019cbdf9803224095
umarless <umarlesss@gmail.com>
2024-06-21 15:53:20 +0530

    fix: actual code for igst assessable value

--- nirjay_customization/override/purchase_order.py
+++ nirjay_customization/override/purchase_order.py
@@ -1,4 +1,15 @@
 import frappe
 
 def before_save(doc, method=None):
-    frappe.errprint(doc.name)
+    fi_percent = doc.custom_freight__insurance_
+
+    for item in doc.items:
+        item.custom_qty_in_pcs = item.custom_pack * item.qty
+        item.custom_rate_per_pcs = item.rate / item.custom_pack
+        # calculation of assessable value
+        fi = item.custom_rate_per_pcs * fi_percent / 100
+        total_rate = item.custom_rate_per_pcs + fi
+        total_amount = total_rate * item.custom_qty_in_pcs
+        custom_duty = total_amount * item.custom_basic_duty_rate / 100
+        sws = custom_duty * 10 / 100
+        item.custom_igst_assessable_value =  total_amount + custom_duty + sws
