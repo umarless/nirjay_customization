cb67dae1f15b68171e9f8d13d05f21c72af731fd
Umar Khan <umarlesss@gmail.com>
2024-06-28 16:49:39 +0530

    fix: adding total IGST assessable value field in PO again

--- nirjay_customization/hooks.py
+++ nirjay_customization/hooks.py
@@ -237,7 +237,6 @@ fixtures = [
                 "Purchase Order Item-custom_basic_duty_rate",
                 "Purchase Order Item-custom_pack",
                 "Purchase Order Item-custom_igst_assessable_value",
-                "Purchase Order-custom_column_break_hf6vn",
                 "Purchase Order-custom_total_igst_assessable_value"
             ]
         ]
