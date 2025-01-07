9fe40e8fc0488b9752919fc2aa70132e47f183fd
Pragati <pragati@mail.hybrowlabs.com>
2025-01-07 13:09:29 +0530

    |nirjay_customization|purchase invoice connection in mpo|

--- nirjay_customization/override/multi_purchase_order_dashboard.py
+++ nirjay_customization/override/multi_purchase_order_dashboard.py
@@ -2,17 +2,17 @@ import frappe
 from frappe import _
 #connection Shipment in sales order
 def get_dashboard_data(data):
-    data["non_standard_fieldnames"]["Sales Invoice"] = "multi_purchase_order"
+    data["non_standard_fieldnames"]["Purchase Invoice"] = "multi_purchase_order"
     
    
     for transaction in data.get("transactions", []):
-        if transaction.get("label") == _("Fulfillment"):
-            transaction["items"].append("Sales Invoice")
+        if transaction.get("label") == _("Connection"):
+            transaction["items"].append("Purchase Invoice")
             break
     else:
         data["transactions"].append({
-            "label": _("Fulfillment"),
-            "items": ["Sales Invoice"],
+            "label": _("Connection"),
+            "items": ["Purchase Invoice"],
         })
     
     return data
